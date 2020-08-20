7c2d2a674d8bc895741b75877b5f2182b4e51e18
hfz1405 <hfznaufal1405@gmail.com>
2020-08-20 21:08:36 +0700

    fixed formating

--- Session 01/canteen-menu.cpp
+++ Session 01/canteen-menu.cpp
@@ -7,46 +7,46 @@ int main(){
 	char nama[50];
 	
 	printf("\t\t\t %s\n", greeting);
-	printf("\t\t\t====================\n\n");
+	printf("\t\t\t===========================\n\n");
 	
-	printf("Input Your Name : ");
+	printf("Input Your Name\t: ");
 	
 	scanf("%[^\n]s", nama);
 	fflush(stdin);
 	
-	printf("\n\n\n Hai %s,\n\n", nama);
-	printf(" MENU [Press 0 if u dont want to order] :");
+	printf("\n\n\nHai %s,\n\n", nama);
+	printf("MENU [Press 0 if u dont want to order]");
 	
-	printf("\n\n Fried Fries Rp.20.000 : ");
+	printf("\n\nFried Fries Rp.20.000\t: ");
 	scanf("%d", &fri);
 	
-	printf(" Dougnat	Rp.15.000 : ");
+	printf("Dougnat	Rp.15.000\t: ");
 	scanf("%d", &dnut);
 	
-	printf(" Burger	Rp.18.000 : ");
+	printf("Burger	Rp.18.000\t: ");
 	scanf("%d", &burg);
 	
-	printf(" Tea	Rp.7.500 : ");
+	printf("Tea	Rp.7.500\t: ");
 	scanf("%d", &tea);
 	
-	printf(" Coffee	Rp.10.000 : ");
+	printf("Coffee	Rp.10.000\t: ");
 	scanf("%d", &cof);
 	
-	printf(" Juice	Rp.15.000 : ");
+	printf("Juice	Rp.15.000\t: ");
 	scanf("%d", &jui);
 	
 	getchar();
 	
 	printf("\n\nPurchase List : \n");
-	printf(" Fried Fries : %d = Rp.%d\n", fri, fri*20000);
-	printf(" Dougnat : %d = Rp.%d\n", dnut, dnut*15000);
-	printf(" Burger : %d = Rp.%d\n", burg, burg*18000);
-	printf(" Tea : %d = Rp.%d\n", tea, tea*7500);
-	printf(" Coffee : %d = Rp.%d\n", cof, cof*10000);
-	printf(" Juice : %d = Rp.%d\n", jui, jui*15000);
+	printf("Fried Fries\tx %d = Rp.%d\n", fri, fri*20000);
+	printf("Dougnat\t\tx %d = Rp.%d\n", dnut, dnut*15000);
+	printf("Burger\t\tx %d = Rp.%d\n", burg, burg*18000);
+	printf("Tea\t\tx %d = Rp.%d\n", tea, tea*7500);
+	printf("Coffee\t\tx %d = Rp.%d\n", cof, cof*10000);
+	printf("Juice\t\tx %d = Rp.%d\n", jui, jui*15000);
 	
 	jumlah = (fri*20000)+(dnut*15000)+(burg*18000)+(tea*7500)+(cof*10000)+(jui*15000);
-	printf("\n Total : Rp.%d\n\n", jumlah);
+	printf("\nTotal\t\t= Rp.%d\n\n", jumlah);
 	getchar();
 	
 	printf("\n Thankyou for Coming...");
